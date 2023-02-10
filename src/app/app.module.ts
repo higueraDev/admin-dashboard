5eb014c085452b037ff12566217c8c6e221086ce
higueraDev <49871959+higueraDev@users.noreply.github.com>
2023-02-10 15:14:33 -0700

    [ADDED]: routing system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graphic1Component } from './pages/graphic1/graphic1.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -20,11 +23,13 @@ import { HeaderComponent } from './shared/header/header.component';
     BreadcrumbsComponent,
     SidebarComponent,
     HeaderComponent,
+    ProgressComponent,
+    Graphic1Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
