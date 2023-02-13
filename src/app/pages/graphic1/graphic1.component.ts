11b64f64dd483f70001956a4000ebf3faeb85306
higueraDev <49871959+higueraDev@users.noreply.github.com>
2023-02-13 16:30:37 -0700

    [UPDATED]: graphic page donuts components

--- src/app/pages/graphic1/graphic1.component.ts
+++ src/app/pages/graphic1/graphic1.component.ts
@@ -12,4 +12,22 @@ export class Graphic1Component {
     'Mail-Order Sales',
   ];
   public data1: number[] = [300, 300, 300];
+  public labels2: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public data2: number[] = [11, 223, 22];
+  public labels3: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public data3: number[] = [33, 12, 12];
+  public labels4: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public data4: number[] = [334, 30, 345];
 }
